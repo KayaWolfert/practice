d4f5068355b8c02c236d2317903904156a9cb080
Kaya Wolfert <kayawolfert@web.de>
2024-05-21 18:50:27 +0200

    small changes

--- components/first-section.js
+++ components/first-section.js
@@ -1,13 +1,10 @@
 import styled from "styled-components";
 import { fruitarray } from "@/lib/data";
 
+export default function RenderSection1({ fruit }) {
+  const { title, description, icon, color } = fruit;
 
-export default function RenderSection1({fruit}) {
-    
-    const { title, description, icon, color } = fruit;
-    
-
-    const StyledSection = styled.section`
+  const StyledSection = styled.section`
     background-color: ${color};
     width: 90%;
     margin-left: auto;
@@ -16,33 +13,33 @@ export default function RenderSection1({fruit}) {
     height: 200px;
   `;
 
-   
-    
-    
-    
-    return ( <> <StyledH1> {title} </StyledH1>
-    <StyledSection>
-      <StyledIcon> {icon}</StyledIcon>
-    </StyledSection>
-
-    <StyledDescription> {description}</StyledDescription></>)
+  return (
+    <>
+      {" "}
+      <StyledH1> {title} </StyledH1>
+      <StyledSection>
+        <StyledIcon> {icon}</StyledIcon>
+      </StyledSection>
+      <StyledDescription> {description}</StyledDescription>
+    </>
+  );
 }
 
 const StyledIcon = styled.div`
-font-size: 8em;
-text-align: center;
-padding: 20px;
+  font-size: 8em;
+  text-align: center;
+  padding: 20px;
 `;
 
 const StyledH1 = styled.h1`
-text-align: center;
-font-size: 3rem;
+  text-align: center;
+  font-size: 3rem;
 `;
 
 const StyledDescription = styled.p`
-width: 90%;
-margin-left: auto;
-margin-right: auto;
-line-height: 1.3;
-margin-top: 20px;
+  width: 90%;
+  margin-left: auto;
+  margin-right: auto;
+  line-height: 1.3;
+  margin-top: 20px;
 `;
--- components/second-section.js
+++ components/second-section.js
@@ -1,58 +1,50 @@
 import styled from "styled-components";
 
+export default function RenderSection2({
+  fruit,
+  increase,
+  decrease,
+  count,
+  foodView,
+}) {
+  const { title, description, icon, color } = fruit;
 
-export default function RenderSection2({fruit, increase, decrease, count, foodView}){
-
-    const { title, description, icon, color } = fruit;
- 
-   
-  
-
-  
-  
-
-  return ( 
+  return (
     <>
-    <StyledSection2>
-    <StyledButton onClick={increase}> Add {title} </StyledButton>
-    <StyledDisplay>{count}</StyledDisplay>
-    <StyledButton onClick={decrease}> Delete {title} </StyledButton>
-    </StyledSection2>
-    
-    <ItemContainer>
-      <h3> Your {title}:</h3>
-      <StyledItem> {foodView}</StyledItem>
-      
-       </ItemContainer>
-       </>
-  )
+      <StyledSection2>
+        <StyledButton onClick={increase}> Add {title} </StyledButton>
+        <StyledDisplay>{count}</StyledDisplay>
+        <StyledButton onClick={decrease}> Delete {title} </StyledButton>
+      </StyledSection2>
+
+      <ItemContainer>
+        <h3> Your {title}:</h3>
+        <StyledItem> {foodView}</StyledItem>
+      </ItemContainer>
+    </>
+  );
 }
 
 const StyledItem = styled.span`
-font-size :2em;
-
+  font-size: 2em;
 `;
 
 const StyledSection2 = styled.section`
-width: 90%;
-margin-left: auto;
-margin-right: auto;
-border-radius: 10px;
-display: flex;
-justify-content: space-between;
+  width: 90%;
+  margin-left: auto;
+  margin-right: auto;
+  border-radius: 10px;
+  display: flex;
+  justify-content: space-between;
 `;
 
-
-
-
-
 const StyledButton = styled.button`
-padding: 10px;
-width: 120px;
+  padding: 10px;
+  width: 120px;
 `;
 
 const StyledDisplay = styled.div`
-font-size: 2em;
+  font-size: 2em;
 `;
 
 const ItemContainer = styled.div`
@@ -60,6 +52,4 @@ const ItemContainer = styled.div`
   margin-top: 30px;
   margin-left: auto;
   margin-right: auto;
-
-
 `;
